b1e090e22f2324c8942f2c1637876f0775150a08
khaled-farahat <khaled-farahat@users.noreply.github.com>
2023-09-26 10:51:14 +0000

    deploy: 01f848cab78fb7d5fa88239ecaf7462a206b6342

---
+++ assets/PageNotFound-4b92212f.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-87d6d806.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/Product.module-42be44cb.js
@@ -0,0 +1,1 @@
+const t="_product_pu6v3_1",o={product:t};export{o as s};
